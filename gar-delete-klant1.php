1e168dda417884ca0072df779f989b276906519f
Nigel Roosenburg <9010998@student.zadkine.nl>
2021-01-06 13:51:57 +0100

    first commit

---
+++ gar-delete-klant1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Anjo Eijeriks">
+    <title>gar-delete-klant1.php</title>
+</head>
+<body>
+    <h1>garage delete klant 1</h1>
+    <p>
+        Dit formulier zoekt een klant op uit
+        de tabel klanten van database garage
+        om hem te kunnen verwijderen.
+    </p>
+    <form action="gar-delete-klant2.php" method="POST">
+        Welk klantid wilt u verwijderen?
+        <input type="text" name="klantidvak"> <br />
+        <input type="submit">
+    </form>
+</body>
+</html>
---
+++ gar-search-klant1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Anjo Eijeriks">
+    <title>gar-search-klant1.php</title>
+</head>
+<body>
+    <h1>Garage zoek op klantid 1</h1>
+    <p>
+        Dit formulier zoekt een klant op uit
+        de tabel klanten van database garage.
+    </p>
+    <form action="gar-search-klant2.php" method="post">
+        Welke klantid zoekt u?
+        <input type="text" name="klantidvak"> <br />
+        <input type="submit">
+    </form>
+</body>
+</html>
